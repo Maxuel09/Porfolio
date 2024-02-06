12770de727bc11a94a77d5278ae40c7a4ff4a524
Maxuel09 <maxuelmorillocuevas@gmail.com>
2024-02-06 17:49:01 +0100

    upgrade vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: vercel(),
+    adapter: vercel({
+        imageService: true,
+        devImageService: 'squoosh',
+    }),
 });
